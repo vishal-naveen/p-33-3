109ccd3672097353be2f975c677023eda89727f8
vishal-naveen <71558475+vishal-naveen@users.noreply.github.com>
2021-06-25 15:16:26 -0400

    Add files via upload

---
+++ p-33-2-main/sketch.js
@@ -0,0 +1,100 @@
+const Engine = Matter.Engine,
+ World = Matter.World,
+ Events = Matter.Events,
+ Bodies = Matter.Bodies;
+ 
+var particles = [];
+var plinkos = [];
+var divisions = [];
+
+var scores = 0; 
+
+var divisionHeight=300;
+var score =0;
+function setup() {
+  createCanvas(800, 800);
+  engine = Engine.create();
+  world = engine.world;
+  ground = new Ground(width/2,height,width,20);
+
+
+   for (var k = 0; k <=width; k = k + 80) {
+     divisions.push(new Divisions(k, height-divisionHeight/2, 10, divisionHeight));
+   }
+
+
+    for (var j = 75; j <=width; j=j+50) 
+    {
+    
+       plinkos.push(new Plinko(j,75));
+    }
+
+    for (var j = 50; j <=width-10; j=j+50) 
+    {
+    
+       plinkos.push(new Plinko(j,175));
+    }
+
+     for (var j = 75; j <=width; j=j+50) 
+    {
+    
+       plinkos.push(new Plinko(j,275));
+    }
+
+     for (var j = 50; j <=width-10; j=j+50) 
+    {
+    
+       plinkos.push(new Plinko(j,375));
+    }
+
+    
+
+    
+}
+ 
+
+
+function draw() {
+  background("black");
+  textSize(20)
+ //text("Score : "+score,20,30);
+  Engine.update(engine);
+  if(particles.y === 100){
+     score += 100
+  }
+  text("Score: " + score, 50,50)
+ 
+  
+   for (var i = 0; i < plinkos.length; i++) {
+     
+     plinkos[i].display();
+     
+   }
+   if(frameCount%60===0){
+     particles.push(new Particle(random(width/2-30, width/2+30), 10,10));
+   //  score++
+   }
+ 
+  for (var j = 0; j < particles.length; j++) {
+   
+     particles[j].display();
+   }
+   for (var k = 0; k < divisions.length; k++) {
+     
+     divisions[k].display();
+   }
+   textSize(25);
+   text("500",20,520);
+   text("500",100,520);
+   text("500",180,520);
+   text("100",260,520);
+   text("100",340,520);
+   text("100",420,520);
+   text("100",500,520);
+   text("200",580,520);
+   text("200",660,520);
+   text("200",740,520);
+
+   if(particles.x )
+
+}
